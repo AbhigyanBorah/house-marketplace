e72b326fc7bdd1cb3b900b3d2ffd08fcdf75783e
Abhigyan <abhigyanritiz63@gmail.com>
2022-05-07 23:42:49 +0530

    authentication

---
+++ src/components/PrivateRoutes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
+
+function PrivateRoutes() {
+    const { loggedIn, checkingStatus } = useAuthStatus()
+
+    if (checkingStatus) {
+        return <Spinner />
+    }
+
+
+    return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />
+}
+
+export default PrivateRoutes
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,119 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import OAuth from '../components/OAuth'
+import { doc, setDoc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false)
+  // instead of having two separate states for email and pasword, we have one state as formData object having both email and password
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+
+  // to use these objects anywhere we have to desrtucture it
+  const { name, email, password } = formData
+
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      /*  email:e.target.value,
+       password:e.target.value
+       OR  */
+      [e.target.id]: e.target.value
+    }))
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user=userCredential.user
+
+      updateProfile(auth.currentUser,{
+        displayName:name
+      })
+
+      const formDataCopy={...formData}
+      delete formDataCopy.password
+      formDataCopy.timestamp=serverTimestamp()
+
+      await setDoc(doc(db,'users',user.uid),formDataCopy)
+
+      navigate('/')
+    } catch (error) {
+      toast.error('Something went wrong with registration')
+    }
+  }
+
   return (
-    <div>
-       <h1>Sign Up</h1> 
-    </div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">
+            Welcome!
+          </p>
+        </header>
+
+        <form onSubmit={onSubmit}>
+          <input type="text"
+            className='nameInput'
+            placeholder='Name'
+            id='name'
+            value={name}
+            onChange={onChange}
+          />
+
+          <input type="email"
+            className='emailInput'
+            placeholder='Email'
+            id='email'
+            value={email}
+            onChange={onChange}
+          />
+
+          <div className="passwordInputDiv">
+            <input type={showPassword ? 'text' : 'password'}
+              className='passwordInput'
+              placeholder='Password'
+              id='password'
+              value={password}
+              onChange={onChange}
+            />
+
+            <img src={visibilityIcon} alt="show passowrd" className='showPassword' onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+
+          <div className="signUpBar">
+            <p className="signUpText">
+              Sign Up
+            </p>
+            <button className='signUpButton'>
+              <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+
+        <OAuth />
+
+        <Link to='/sign-in' className='registerLink'>
+          Sign In Instead
+        </Link>
+      </div>
+    </>
   )
 }
 
